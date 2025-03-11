82eb42ded68f5ac7b0adb40f6c0359fb9d2bef44
Fathi Al-Sadi <fathii.alsadi@gmail.com>
2025-03-11 10:53:17 +0300

    add

--- php project/crud_php/Partials/nav.php
+++ php project/crud_php/Partials/nav.php
@@ -2,12 +2,12 @@
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#"> <img src="images/Solvex Logo.png"></a>
+      <a class="navbar-brand" href="Home.php"> <img src="images/Solvex Logo.png"></a>
     </div>
     <ul class="nav navbar-nav">
       <li class="active"><a href="Home.php">Home</a></li>
-      <li><a href="#">Page 1</a></li>
-      <li><a href="#">Page 2</a></li>
+      <li><a href="Posts.php">Posts</a></li>
+      <!-- <li><a href="#">Page 2</a></li> -->
       <li><a href="profile.php">Profile</a></li>
     </ul>
   </div>
