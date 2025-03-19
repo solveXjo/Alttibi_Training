a4958cbf96b8aac299556666f170f3028ee132fe
Solvex <fathii.alsadi@gmail.com>
2025-03-19 13:27:50 +0300

    added

--- php_project/crud_php/config.php
+++ php_project/crud_php/config.php
@@ -4,7 +4,7 @@ return [
         'host' => 'localhost',
         'port' => '3306',
         'dbname' => 'crud',
-        'charset' => 'utf8',
+        'charset' => 'utf8mb4',
         'username' => 'root',
         'password' => '',
     ]
