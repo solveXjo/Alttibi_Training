6376cfa9cb1cf7510d9eb90429a9c44521579e8f
Solvex <fathii.alsadi@gmail.com>
2025-04-08 16:48:30 +0300

    added

--- php_project/crud_php/Home.view.php
+++ php_project/crud_php/Home.view.php
@@ -14,27 +14,6 @@ if (!isset($_SESSION['user_id'])) {
     <title>Home</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <style>
-
-
-
-
-
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-    </style>
 </head>
 
 <body>
